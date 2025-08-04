1a32929dd49519243aaf0d8c817487c2114c3550
HimayaPrevention <himaya.prevention@gmail.com>
2025-08-04 23:41:36 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'utils/theme.dart';
+import 'screens/splash_screen.dart';
+import 'screens/product_list_screen.dart';
+import 'screens/cart_screen.dart';
+import 'screens/order_detail_screen.dart';
+import 'screens/booking_form.dart';
+import 'screens/incoming_requests.dart';
+import 'screens/feedback_form.dart';
+import 'screens/notification_screen.dart';
+import 'screens/admin_panel.dart';
+import 'services/mock_data.dart';
+
+// ... rest of the code as shown above ...
